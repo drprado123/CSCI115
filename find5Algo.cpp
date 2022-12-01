7e20919329e77b61ac51621406a71ec7e783c33e
drprado123 <103281716+drprado123@users.noreply.github.com>
2022-11-30 17:32:31 -0800

    update find5

--- find5Algo.cpp
+++ find5Algo.cpp
@@ -3,11 +3,23 @@ using namespace std;
 
 bool hashcheck(int* hash, int val)
 {
+    if (val % 5 <= 0 && val <= 5)
+    {
+        hash[val]++;
+    }
+    if (hash[0] >= 1 && hash[5] >= 1) return true;
+    if (hash[1] >= 1 && hash[4] >=1) return true;
+    if (hash[2] >=1 && hash[3]>=1) return true;
+    else return false;
 
 }
 bool find5hash (int* vals, int size)
 {
     int* hashing  = new int(6);
+    for (int i = 0; i < 6; i++)
+    {
+        hashing[i] = 0;
+    }
     int i = 0;
     while (i < size)
     {
