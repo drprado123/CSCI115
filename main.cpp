4156bf2f8cafeadf26e73c765700f191ae2e4d60
drprado123 <103281716+drprado123@users.noreply.github.com>
2022-12-01 11:19:44 -0800

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <chrono
+#include <vector>
+#include "Sorting_Algos.cpp"
+using namespace std;
+
+
+//This is the main where all tests and logging will be performed
+int main()
+{
+  return 0;
+}
