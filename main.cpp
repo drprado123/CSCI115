5961f591ec8d92ea21b67cb852a6cd6850ebcc87
drprado123 <103281716+drprado123@users.noreply.github.com>
2022-12-01 14:16:48 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 #include <chrono
 #include <vector>
 #include "Sorting_Algos.cpp"
@@ -8,5 +9,15 @@ using namespace std;
 //This is the main where all tests and logging will be performed
 int main()
 {
+    int* random_arr;
+    int* sorted_arr;
+    int* reverse_arr;
+    int* holder;
+  for (int i = 0 ; i< 1000; i++)
+  {
+    
+  
+  }
+  
   return 0;
 }
