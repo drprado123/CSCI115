20ca2f9fc3469cac9e43fef04cd5b690bb30c318
drprado123 <103281716+drprado123@users.noreply.github.com>
2022-12-04 11:57:08 -0800

    update

--- find5Algo.cpp
+++ find5Algo.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include "Sorting_Algos.cpp"
 using namespace std;
 
 bool hashcheck(int* hash, int val)
@@ -23,8 +24,19 @@ bool find5hash (int* vals, int size)
     int i = 0;
     while (i < size)
     {
-        if (hashcheck(hashing,vals[i]) == true) return true;
+        if (vals[i] % 5 <= 0 && vals[i] <= 5)
+        {
+            hashing[vals[i]]++;
+        }
+        if (hashing[0] >= 1 && hashing[5] >= 1) return true;
+        if (hashing[1] >= 1 && hashing[4] >=1) return true;
+        if (hashing[2] >=1 && hashing[3]>=1) return true;
         i++;
     }
     return false;
+}
+
+int main()
+{
+    return 0;
 }
