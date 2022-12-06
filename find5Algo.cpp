df2de9a94c375a576c8a9f985ae0156863c734ed
drprado123 <103281716+drprado123@users.noreply.github.com>
2022-12-05 18:01:55 -0800

    update
    removed unecessary function

--- find5Algo.cpp
+++ find5Algo.cpp
@@ -2,18 +2,6 @@
 #include "Sorting_Algos.cpp"
 using namespace std;
 
-bool hashcheck(int* hash, int val)
-{
-    if (val % 5 <= 0 && val <= 5)
-    {
-        hash[val]++;
-    }
-    if (hash[0] >= 1 && hash[5] >= 1) return true;
-    if (hash[1] >= 1 && hash[4] >=1) return true;
-    if (hash[2] >=1 && hash[3]>=1) return true;
-    else return false;
-
-}
 bool find5hash (int* vals, int size, int findme)
 {
     int* hashing  = new int(findme);
